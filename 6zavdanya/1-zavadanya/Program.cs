c8153662fad83272879f823151e5ab5277912af8
shvagr0 <vladislav.rozvadovski@kpk-lp.com.ua>
2021-11-08 22:12:51 +0200

    add home work

---
+++ 6zavdanya/1-zavadanya/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_zavadanya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть значення першого елемента арифметичної прогресiї: ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введiть значення другого елемента арифметичної прогресiї: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            double progres = x1 - x0;
+
+            byte n = Convert.ToByte(Console.ReadLine());
+            double sum = 0;
+            double result = x0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += result;
+                result += progres;
+            }
+
+            Console.WriteLine("Сума перших {0} елементiв арифметичної прогресiї = {1}", n, sum);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 6zavdanya/3-zavdanya/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_zavdanya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte month = Convert.ToByte(Console.ReadLine());        //ввід кількості місяців
+            double cash = Convert.ToDouble(Console.ReadLine());     //ввід суми грошей які є в банку
+
+            double money = cash;
+
+            /* цикл який обчислює баланс після n кількості місяців */
+            for (int i = 1; i <= month; i++)
+            {
+                cash *= 1.07;
+            }
+
+            Console.WriteLine("За {0} мiсяцiв баланс збiльшився на {1} грн", month, cash - money);
+            Console.ReadKey();
+        }
+    }
+}
